af0d5f086d4a795ced7ba160ffb23577aecff1c7
Liam Butterworth <liam@Liams-MBP.attlocal.net>
2017-12-30 21:31:32 -0600

    update install.sh; install vim plug, vim plugins, zplug and zsh plugins

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-cp ./{.zshrc,.vimrc,.tmux.conf} ~
+
+# copy config files to home directory
+cp ./{.zshrc,.vimrc,.tmux.conf,.tmux.conf} ~
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+
+# install vim plugins
+vim +PlugInstall +qall
+
+# install zsh plugins
+zplug install
