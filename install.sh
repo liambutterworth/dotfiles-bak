553b1e6e962da0c5f8ef7e360cbe970362f947fe
Liam Butterworth <wbbutterworth@gmail.com>
2018-01-10 14:16:45 -0600

    update install script; still testing with docker

--- install.sh
+++ install.sh
@@ -5,12 +5,16 @@
 cp ./{.zshrc,.vimrc,.tmux.conf} ~
 
 # install vim-plug and plugins
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# vim +PlugInstall +qall
 
 # install antigen and plugins
 # mkdir -p ~/.zsh && curl -L git.io/antigen > ~/.zsh/antigen.zsh
 
 # install zplug and plugins
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh || true
+# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh || true
+# curl -fLo ~/.zplug --create-dirs https://raw.githubusercontent.com/zplug/
+# mkdir -p ~/.zplug && git clone https://github.com/zplug/zplug ~/.zplug
+export ZPLUG_HOME=~/.zplug
+git clone https://github.com/zplug/zplug $ZPLUG_HOME
 zplug install
